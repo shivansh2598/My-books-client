97f93c7210c59444d2da812aa74116564670b8b3
shivansh2598 <shivanshsinha2598@gmail.com>
2019-10-06 07:49:14 +0530

    Books and Book js files updated

---
+++ src/components/Book.js
@@ -0,0 +1,44 @@
+import React , {Component} from 'react'
+import { books } from '../data'
+
+const Book = ({book})=>{
+        return (
+            <div>
+                <table className="table table-striped">
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Title</th>
+                            <th>Author</th>
+                            <th>Year</th>
+                            <th>Actions</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            books.map((book)=>{
+                                return (
+                                    <tr key={book.id}>
+                                        <td>{book.id}</td>
+                                        <td>{book.title}</td>
+                                        <td>{book.author}</td>
+                                        <td>{book.year}</td>
+                                        <td>
+                                            <button type="button" className="btn btn-danger">
+                                                Delete
+                                            </button>
+                                            <button type="button" className="btn">
+                                                Edit
+                                            </button>
+                                        </td>
+                                    </tr>
+                                )
+                            })
+                        }
+                    </tbody>
+                </table>
+            </div>
+        )
+    }
+
+export default Books;
